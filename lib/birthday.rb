9c4feafdbb225a5e259455b4f07cc4a96e875237
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2018-11-09 16:30:47 -0800

    Add comma to solution code

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    puts "Happy Birthday, #{kids_name}! You are now #{age} years old!"
   end
 end
 
