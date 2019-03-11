0ed8654eaf2b6a4848bf5e8732e596ed1ac046c7
maxwellbenton <maxwellbenton@gmail.com>
2019-03-11 08:19:02 -0400

    fixes solution, removes bonus method

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,25 +1,11 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday, #{kids_name}! You are now #{age} years old!"
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
-
-## bonus
-
-def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    if age <= 12
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    else
-      puts "You are too old for this."
-    end
-  end
-end
-
-
