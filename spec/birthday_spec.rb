b89651ee2d145cbeec75ed460932bde5dc5a0baa
maxwellbenton <maxwellbenton@gmail.com>
2019-03-11 08:32:31 -0400

    adds bonus test with pending tag

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -1,14 +1,28 @@
 require_relative "../lib/birthday.rb"
 
-describe "#happy_birthday" do 
-  it "prints out a birthday message to each kid in the birthday_kids hash" do 
+describe "#happy_birthday" do
+  it "prints out a birthday message to each kid in the birthday_kids hash" do
     birthday_kids = {
-      "Timmy" => 9, 
-      "Sarah" => 6, 
+      "Timmy" => 9,
+      "Sarah" => 6,
       "Amanda" => 27
     }
 
     expect{happy_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nHappy Birthday Amanda! You are now 27 years old!\n").to_stdout
+
+  end
+end
+
+## BONUS ##
+describe "#age_appropriate_birthday" do
+  pending it "only prints the birthday greeting if the birthday kid 12 or younger" do
+    birthday_kids = {
+      "Timmy" => 9,
+      "Sarah" => 6,
+      "Amanda" => 27
+    }
+
+    expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
 
   end
 end
