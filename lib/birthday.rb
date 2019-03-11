98c6b2f8151b2d779e47f8fa73016206d639df47
maxwellbenton <maxwellbenton@gmail.com>
2019-03-11 08:25:00 -0400

    adds bonus back in

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,15 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+## bonus
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
+end
