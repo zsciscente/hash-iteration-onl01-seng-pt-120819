ec0f7fe582d51886f64e03920ea7dae4e37c9029
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 21:04:14 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,6 +14,8 @@ def age_appropriate_birthday(birthday_kids)
 birthday_kids.each do |kids_name, age|
   if age < 10
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  else
+  end
   end
   
   
